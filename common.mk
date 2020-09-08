0533fa5b654c705d6882a337ae5378af3752acbd
adamychmeizux8 <59512479+adamychmeizux8@users.noreply.github.com>
2020-09-08 14:34:31 +0300

    mokee > lineage

--- common.mk
+++ common.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/meizu/sm6150-common/sm6150-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-mokee
+    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
